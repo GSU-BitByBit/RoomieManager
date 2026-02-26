7f8026eed231f2063c879d107a2bca8b9bb358b9
Uddhav <uddhav3114@gmail.com>
2026-02-26 13:15:50 -0500

    Test

--- frontend/Uddhav_Cota_Frontend/create-group/create-group.js
+++ frontend/Uddhav_Cota_Frontend/create-group/create-group.js
@@ -6,7 +6,34 @@ document.getElementById("createBtn").onclick = () => {
     return
   }
 
-  alert("Group created (mock)")
+  document.getElementById("createBtn").onclick = async () => {
+    const token = localStorage.getItem("accessToken")
+    const name = document.getElementById("groupName").value
 
-  window.location.href = "../app-shell/app-shell.html"
+    if (!name) {
+      alert("Enter group name")
+      return
+    }
+
+    const res = await fetch("http://localhost:3000/api/v1/groups", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
+      },
+      body: JSON.stringify({ name })
+    })
+
+    const body = await res.json()
+
+    if (!body.success) {
+      alert(body.error.message)
+      return
+    }
+
+    // Save group ID
+    localStorage.setItem("currentGroupId", body.data.id)
+
+    window.location.href = "../app-shell/app-shell.html"
+  }
 }
